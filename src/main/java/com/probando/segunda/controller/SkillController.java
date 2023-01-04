5dc207c10cc2735ba206ac9ecd37ea4841b5789e
Sofi <dornasofia@gmail.com>
2023-01-04 18:12:55 -0300

    Imagenes funcionando como string en assets

--- src/main/java/com/probando/segunda/controller/SkillController.java
+++ src/main/java/com/probando/segunda/controller/SkillController.java
@@ -3,8 +3,6 @@ package com.probando.segunda.controller;
 
 import com.probando.segunda.model.Skill;
 import com.probando.segunda.service.SkillService;
-import com.probando.segunda.service.UploadImageService;
-import java.io.IOException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,10 +12,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:4200/"})
@@ -26,8 +22,6 @@ public class SkillController {
     @Autowired
     private SkillService skillServ;
     
-    @Autowired
-    private UploadImageService imageServ;
     
     @GetMapping("/skills")
     @ResponseBody
@@ -37,26 +31,7 @@ public class SkillController {
     
     @PostMapping("/skills")
     @ResponseBody
-    public Skill createSkill(@RequestBody Skill skill, @RequestParam(value = "file", required = false) MultipartFile imagen) throws IOException{
-        
-         if (imagen != null && !imagen.isEmpty()) {
-            String filename = imageServ.copy(imagen);
-            skill.setImagen(filename);
-          }
-        /*if (!imagen.isEmpty()){
-            Path dirImages = Paths.get("src//main//resources//static/images");
-            String rutaAbsoluta = dirImages.toFile().getAbsolutePath();
-            try {
-                byte[] bytesImg = imagen.getBytes();
-                Path rutaCompleta = Paths.get(rutaAbsoluta + "//" + imagen.getOriginalFilename());
-                Files.write(rutaCompleta, bytesImg);
-                skill.setImagen(imagen.getOriginalFilename());
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }*/
-            
-            
-        
+    public Skill createSkill(@RequestBody Skill skill){             
         return skillServ.saveSkill(skill);
     }
     
@@ -67,21 +42,11 @@ public class SkillController {
     }
     
     @PutMapping("/skills/{id}")
-    public Skill editSkill(@PathVariable Long id, @RequestBody Skill skill, @RequestParam("file") MultipartFile imagen) throws IOException{
+    public Skill editSkill(@PathVariable Long id, @RequestBody Skill skill){
         Skill sk= skillServ.findSkill(id);
         sk.setNombre(skill.getNombre());
-        /*sk.setImagen(skill.getImagen());*/
-        sk.setTiposkill(skill.getTiposkill());
-        
-        if (!imagen.isEmpty()) {
-            // Borra la imagen anterior si existe
-            if (sk.getImagen() != null) {
-              imageServ.delete(sk.getImagen());
-            }
-            // Carga la nueva imagen
-                String filename = imageServ.copy(imagen);
-                sk.setImagen(filename);
-            }
+        sk.setImagen(skill.getImagen());
+        sk.setTiposkill(skill.getTiposkill());       
         return skillServ.saveSkill(sk);
     }    
 }
