74de7b08460de6afe91ec050f5ea0ef84000c92c
Sofi <dornasofia@gmail.com>
2023-01-12 02:28:20 -0300

    prueba mail 3

---
+++ src/main/java/com/probando/segunda/model/Email.java
@@ -0,0 +1,12 @@
+
+package com.probando.segunda.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class Email {
+    
+    @Autowired
+    private MailSender mailSender;
+    
+}
