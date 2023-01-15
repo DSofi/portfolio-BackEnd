5b4f1ef3b5efa5e2d0198f0cf02af0e9c59f3e05
Sofi <dornasofia@gmail.com>
2023-01-15 15:14:40 -0300

    Probando

--- src/main/java/com/probando/segunda/SegundaApplication.java
+++ src/main/java/com/probando/segunda/SegundaApplication.java
@@ -1,9 +1,12 @@
 package com.probando.segunda;
 
 import java.util.Properties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -11,10 +14,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SegundaApplication {
+    
+     @Autowired
+    private Environment env;
+    
+    String emailEmail = env.getProperty("EMAIL_EMAIL");
+    String emailPassword = env.getProperty("EMAIL_PASSWORD");
+    String dataSourceUrl = env.getProperty("DATASOURCE_URL");
+    String dataSourceUsername = env.getProperty("DATASOURCE_USERNAME");
+    String dataSourcePassword = env.getProperty("DATASOURCE_PASSWORD");
 
     public static void main(String[] args) {
             SpringApplication.run(SegundaApplication.class, args);
     }
+    
+@Value("${EMAIL_EMAIL}")
+String pruebaEmail;
+
+@Value("${DATASOURCE_URL}")
+String pruebaDataSourceUrl;
+
         
 @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -35,8 +54,8 @@ public class SegundaApplication {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("ap.dsofi@gmail.com");
-        mailSender.setPassword("kmrlhrjpfgpenbrl");
+        mailSender.setUsername(emailEmail);
+        mailSender.setPassword(emailPassword);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
--- src/main/java/com/probando/segunda/service/EmailService.java
+++ src/main/java/com/probando/segunda/service/EmailService.java
@@ -4,6 +4,7 @@ package com.probando.segunda.service;
 import com.probando.segunda.model.Email;
 import com.probando.segunda.model.RestResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.MailSender;
@@ -16,10 +17,15 @@ public class EmailService implements IEmailService{
     @Autowired
     private MailSender mailSender;
     
+    @Autowired
+    private Environment env;
+    
+    String emailEmail = env.getProperty("EMAIL_EMAIL");
+    
     @Override
     public ResponseEntity<RestResponse> enviarEmail(Email email){
         SimpleMailMessage mensaje = new SimpleMailMessage();
-        mensaje.setTo("ap.dsofi@gmail.com");
+        mensaje.setTo( emailEmail);
         mensaje.setFrom(email.getFromEmail());
         mensaje.setSubject("email de : " + email.getNombre());
         mensaje.setText("Mensaje : \n" + email.getMensaje() + "\nde : " + email.getFromEmail());
@@ -27,7 +33,7 @@ public class EmailService implements IEmailService{
         
         SimpleMailMessage confMail = new SimpleMailMessage();
         confMail.setTo(email.getFromEmail());
-        confMail.setFrom("ap.dsofi@gmail.com");
+        confMail.setFrom( emailEmail);
         confMail.setSubject("Su email ha sido enviado correctamente");
         confMail.setText("Esto es un mensaje de confirmación, gracias por ver mi portfolio y escribirme, pronto estaré respondiendo.\n \nSaludos, \nSofi . ");
         mailSender.send(confMail);
